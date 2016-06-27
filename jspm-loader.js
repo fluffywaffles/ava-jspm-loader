d8be77935004cf68f5151898976e3b49cef4a803
fluffywaffles <skorlir@gmail.com>
2016-06-27 06:32:33 -0500

    AVA JSPM loader helper.

---
+++ jspm-loader.js
@@ -0,0 +1,34 @@
+const Module = require('module')
+    , jspm   = require('jspm')
+    , path   = require('path')
+    , url    = require('url')
+    , load   = Module._load
+
+jspm.setPackagePath('..')
+
+const System = jspm.Loader()
+
+// NOTE(jordan): Resolve local project imports correctly.
+function moduleIsInSystemPaths (name) {
+  let systemPath = path.parse(name).dir + '/'
+
+  return systemPath in System.paths
+}
+
+// NOTE(jordan): Shimming Module._load to check jspm_packages.
+Module._load = (name, m) => {
+  // Is the module "special"? (Cannot be looked up in filesystem)
+  // Needed this to fix the errors caused by SpecialSystemModules like @system-env
+  if (name in System._loader.modules) {
+    return System._loader.modules[name].module
+  }
+
+  // Is the module a JSPM dependency?
+  if (name in System.map || moduleIsInSystemPaths(name)) {
+    let jspmUri  = System.normalizeSync(name)
+    name = url.parse(jspmUri).path
+  }
+
+  // Try first to load a JSPM dep, then try local project, then try NPM.
+  return load(name, m)
+}
